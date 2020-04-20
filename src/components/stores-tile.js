b18a1492fc32aacf651c81bbf1ea0a0580182f21
Ashley Rodan <8232214+ashrodan@users.noreply.github.com>
2020-04-20 17:16:28 +1000

    add modal and re-arrage some stuff

--- src/components/stores-tile.js
+++ src/components/stores-tile.js
@@ -10,31 +10,47 @@ import InfoIcon from '@material-ui/icons/Info';
 import { storesCol } from '../stores-firebase'
 import default_shop_icon from './default_shop_icon.png'
 
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import StoreInfoModal from './store-info-modal';
+
 const SORT_OPTIONS = {
     "A-Z": { column: 'name', direction: 'asc' },
     "Postcode": { column: 'delivery_postcodes', direction: 'desc' },
 }
 
-function useStores(sortBy = "A-Z", store_cat_filter) {
+let filter = []
+// function onlyUnique(value, index, self) {
+//     return self.indexOf(value) === index;
+// }
+function useStores(store_cat_filter) {
+    // filter = filter.filter(onlyUnique)
+    // if (store_cat_filter != undefined) {
+    //     if (filter.includes(store_cat_filter)) {
+    //         filter.pop(store_cat_filter)
+    //     } else {
+    //         debugger
+    //         filter.push(store_cat_filter)
+    //     }
+    // }
     const [store, setStores] = useState([])
-    console.log(sortBy);
 
     useEffect(() => {
         const fb_connection = storesCol()
-            .orderBy(SORT_OPTIONS[sortBy].column, SORT_OPTIONS[sortBy].direction)
+            .orderBy(SORT_OPTIONS['A-Z'].column, SORT_OPTIONS['A-Z'].direction)
             .onSnapshot((snapshot) => {
                 let newStores = snapshot.docs.map(doc => ({
                     'id': doc.id,
                     ...doc.data(),
-                })).filter(e => store_cat_filter ? (e.store_cat == 'Bakery') : e)
-                console.log(newStores);
+                })).filter(e => filter.length > 0 ? filter.includes(e.store_cat) : e)
                 setStores(newStores)
             })
 
         // unsubs from FB when leaving this component
         return () => fb_connection()
 
-    }, [sortBy])
+    }, [store_cat_filter])
     return store
 }
 
@@ -47,8 +63,8 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.background.paper,
     },
     gridList: {
-        width: 1000,
-        height: 450,
+        width: '100%',
+        // height: 450,
     },
     icon: {
         color: 'rgba(255, 255, 255, 0.64)',
@@ -57,29 +73,52 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function TitlebarGridList() {
-    const [sortBy, setStoreSort] = useState('A-Z')
-    const stores = useStores(sortBy)
+    const [store_cat_filter, setStoreSort] = useState()
+    const [open_store_info, setStoreInfoModal] = useState(false)
+    const stores = useStores(store_cat_filter)
 
     const classes = useStyles();
     return (
-        <div className={classes.root}>
-            <GridList cellHeight={180} className={classes.gridList}>
-
-                {stores.map((store) => (
-                    <GridListTile key={store.name} cols={.5}>
-                        <img src={store.store_url || default_shop_icon} alt={store.name} />
-                        <GridListTileBar
-                            title={store.name}
-                            subtitle={<span>{store.store_cat}</span>}
-                            actionIcon={
-                                <IconButton aria-label={`info about ${store.name}`} className={classes.icon}>
-                                    <InfoIcon />
-                                </IconButton>
-                            }
-                        />
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
+        <div>
+            {/* filter checkbox */}
+            {/* <FormControlLabel
+                control={<Checkbox
+                    checked={filter.includes('Bakery')}
+                    onChange={() => setStoreSort('Bakery')}
+                    name="checkedA"
+                />}
+                label="Bakery"
+            /> */}
+            <div className={classes.root}>
+                <GridList cellHeight={180} className={classes.gridList} cols={window.innerHeight < 1300 ? 2 : 4}>
+                    {stores.map((store) => (
+                        <GridListTile key={store.name} cols={1}>
+                            <img src={store.img || default_shop_icon} alt={store.name} />
+                            <GridListTileBar
+                                title={store.name}
+                                subtitle={<span>{store.store_cat}</span>}
+                                actionIcon={
+                                    <IconButton
+                                        aria-label={`info about ${store.name}`}
+                                        className={classes.icon}
+                                        onClick={() => setStoreInfoModal(store)}
+                                    >
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div >
+            {
+                // Store Model setup
+                open_store_info &&
+                <StoreInfoModal
+                    open_state={!!open_store_info}
+                    store_info={open_store_info}
+                    handle_close={() => setStoreInfoModal(null)}
+                />}
+        </div >
     );
 }
